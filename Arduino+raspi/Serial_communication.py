d8937befd2fdfbfcf13e86e4a3a61b04dad4169c
Bugates <ntiwari@olin.edu>
2025-11-18 02:28:08 -0500

    Clean up COMMANDS handling in Serial_communication.py
    Removed unnecessary reversal and print of COMMANDS.

--- Arduino+raspi/Serial_communication.py
+++ Arduino+raspi/Serial_communication.py
@@ -16,10 +16,6 @@ COMMANDS = [
     "END"
 ]
 
-COMMANDS = COMMANDS[-2::-1]
-COMMANDS.append("END")
-print(COMMANDS)
-
 def find_ports():
     return glob.glob("/dev/ttyACM*") + glob.glob("/dev/ttyUSB*")
 
